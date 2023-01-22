d8fa1c157f088251977b0f4b3b8908c5e22c526b
JhonatanRaul16 <jhonatanraul16@gmail.com>
2023-01-21 21:31:03 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./components/Header"
+import './App.css'
+function App() {
+  return (
+    <div className="App">
+      {/* Header Section */}
+      <Header/>
+      {/* Main Container */}
+      <main>
+        <div className="mainContainer">
+          
+        </div>
+      </main>
+      {/* Botoom Menu */}
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import { BarChart, SearchRounded, ShoppingCart } from '@mui/icons-material'
+import React from 'react'
+import Logo from '../assets/logo.png'
+import Avatar from '../assets/Avatar.jpg'
+const Header = () => {
+  return (
+    <header>
+        <img 
+        src={Logo} 
+        alt="logo"
+        className='logo'/>
+        <div className="inputBox">
+            <SearchRounded className="searchIcon"/>     
+            <input type="text"  placeholder='Search'/>
+        </div>
+        <div className="shoppingCart">
+            <ShoppingCart className='cart'/>
+            <div className="cart_content">
+                <p>2</p>
+            </div>
+        </div>
+        <div className="profileContainer">
+            <div className="imgBox">
+                <img src={Avatar} alt="" />
+            </div>
+            <h2 className='userName'>Jhonatan P.</h2>
+        </div>
+        <div className="toggleMenu">
+            <BarChart className='toggleIcon'/>
+        </div>
+    </header>
+  )
+}
+
+export default Header
