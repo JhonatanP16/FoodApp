048c4b70f9ab8d149ef8df0147e4b3b2e479f342
JhonatanP16 <jhonatanpanduro16@gmail.com>
2023-01-21 22:18:21 -0500

    avance

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Header from "./components/Header"
 import './App.css'
+import MenuContainer from "./components/MenuContainer"
+import { AccountBalance, AccountBalanceWalletRounded, Chat, Favorite, HomeRounded, Settings, SummarizeRounded } from "@mui/icons-material"
 function App() {
   return (
     <div className="App">
@@ -7,11 +9,27 @@ function App() {
       <Header/>
       {/* Main Container */}
       <main>
-        <div className="mainContainer">
-          
-        </div>
+        <div className="mainContainer"></div>
+        <div className="rightMenu"></div>
       </main>
       {/* Botoom Menu */}
+      <div className="bottomMenu">
+        <ul id="menu">
+            {/* prettier ignore */}
+            <MenuContainer link={'#'} icon ={<HomeRounded/>}/>
+            {/* prettier ignore */}
+            <MenuContainer link={'#'} icon ={<Chat/>}/>
+            {/* prettier ignore */}
+            <MenuContainer link={'#'} icon ={<AccountBalanceWalletRounded/>}/>
+            {/* prettier ignore */}
+            <MenuContainer link={'#'} icon ={<Favorite/>}/>
+            {/* prettier ignore */}
+            <MenuContainer link={'#'} icon ={<SummarizeRounded/>}/>
+            {/* prettier ignore */}
+            <MenuContainer link={'#'} icon ={<Settings/>}/>
+            <div className="indicator"></div>
+        </ul>
+      </div>
     </div>
   )
 }
